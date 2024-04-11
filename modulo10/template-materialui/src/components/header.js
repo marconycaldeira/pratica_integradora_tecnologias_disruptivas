f8bb7aa688ebb6dc63a6eeafdb59dc9bfd8dcaa6
Marcony Caldeira <marconycaldeira@gmail.com>
2024-04-11 16:06:10 -0300

    Update header.js

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -5,31 +5,48 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
- 
+import MenuIcon from '@mui/icons-material/Menu';
+import { styled } from '@mui/system';
+
+const StyledAppBar = styled(AppBar)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+});
+
+const StyledTitle = styled(Typography)({
+  flexGrow: 1,
+});
+
+const StyledButton = styled(Button)({
+  background: 'white',
+  color: 'black',
+  '&:hover': {
+    background: 'rgba(255, 255, 255, 0.8)',
+  },
+});
+
 function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <StyledTitle variant="h6" component="div">
+            Gerenciamento de Projetos
+          </StyledTitle>
+          <StyledButton variant="contained">Login</StyledButton>
+        </Toolbar>
+      </StyledAppBar>
+    </Box>
+  );
 }
- 
+
 export default Header;
